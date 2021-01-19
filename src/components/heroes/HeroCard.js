5aaf413fca6163e2ebbc7fda76631c8f9b77dba8
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-01-19 17:25:29 +0100

    better structure folder

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -15,7 +15,7 @@ export const HeroCard = ({
       <div className="card ms-3" style={{maxWidth: 540}}>
         <div className="row no-gutters">
           <div className="col-md-4">
-            <img className="card-img" src={`assets/heroes/${id}.jpg`} alt={superhero}/>
+            <img className="card-img" src={`./assets/${id}.jpg`} alt={superhero}/>
           </div>
           <div className="col-md-8">
             <div className="card-body" >
